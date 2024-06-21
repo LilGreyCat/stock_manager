58613c2ffaf46b534d6b9d087de269c0c4881a75
Jonathan V <pro.jonathanvgr@gmail.com>
2024-06-21 18:23:40 +0200

    Updated and fonctionnal Product Controller

--- back-end/controllers/product.go
+++ back-end/controllers/product.go
@@ -1,64 +1,85 @@
 package controllers
 
 import (
-	"net/http"
 	"stock_manager_back-end/database"
 	"stock_manager_back-end/models"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 type ProductService struct{}
 
-func (s *ProductService) Get(c *gin.Context) {
+func (s ProductService) Get(c *gin.Context) {
+	var gorm_res *gorm.DB
 	var product []models.Product
 	id := c.Query("id")
+
 	if id == "" {
-		database.DB.Find(&product)
+		gorm_res = database.DB.Find(&product)
+	} else {
+		gorm_res = database.DB.First(&product, id)
 	}
 
-	c.JSON(200, gin.H{
-		"data": product,
-	})
+	if gorm_res.Error != nil {
+		Send_Internal_Error(c, gorm_res.Error)
+	}
+
+	Send_OK(c, product)
 }
 
-func (s *ProductService) Create(c *gin.Context) {
-	var product models.Product
+func (s ProductService) Create(c *gin.Context) {
+	var gorm_res *gorm.DB
+	var body models.Product
 
-	if err := c.ShouldBindJSON(&product); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	if err := c.ShouldBindJSON(&body); err != nil {
+		Send_Bad_Request(c, err)
 		return
 	}
 
-	res := database.DB.Create(&product)
+	gorm_res = database.DB.Create(&body)
 
-	if res.Error != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": res.Error.Error(),
-		})
+	if gorm_res.Error != nil {
+		Send_Internal_Error(c, gorm_res.Error)
 	}
 
-	c.JSON(200, gin.H{
-		"data": product,
-	})
+	Send_Created_Updated(c, body)
 }
 
 func (s ProductService) Update(c *gin.Context) {
-	//
+	var gorm_res *gorm.DB
+	var product models.Product
+	var body models.Product
+	id := c.Param("id")
+
+	gorm_res = database.DB.First(&product, id)
+	if gorm_res.Error != nil {
+		Send_Bad_Request(c, gorm_res.Error)
+	}
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		Send_Bad_Request(c, err)
+		return
+	}
+
+	gorm_res = database.DB.Model(&product).Updates(&body)
+	if gorm_res.Error != nil {
+		Send_Internal_Error(c, gorm_res.Error)
+	}
+
+	Send_Created_Updated(c, product)
 }
 
 func (s ProductService) Delete(c *gin.Context) {
+	var gorm_res *gorm.DB
 	id := c.Param("id")
 
-	res := database.DB.Unscoped().Delete(&models.Product{}, id)
+	// Unscoped() to hard delete, might change later
+	gorm_res = database.DB.Unscoped().Delete(&models.Product{}, id)
 
-	if res.Error != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": res.Error.Error(),
-		})
+	if gorm_res.Error != nil {
+		Send_Bad_Request(c, gorm_res.Error)
 	}
 
-	c.JSON(200, gin.H{
-		"data": "Delete ok",
-	})
+	Send_OK(c, "Product deleted")
 }
