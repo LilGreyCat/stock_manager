9fe1036e840fdedb52b590cc9eefff58369dfaaf
Jonathan V <pro.jonathanvgr@gmail.com>
2024-07-01 12:01:15 +0200

    Removed not null constraint

--- back-end/models/product.go
+++ back-end/models/product.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type Product struct {
 	gorm.Model
 	Name          string `gorm:"not null" json:"name"`
-	ProductTypeID uint   `gorm:"not null" json:"type"`
 	Stock         uint   `json:"stock" type:"mediumint unsigned"`
+	ProductTypeID uint   `json:"type"`
 	UnitID        uint   `json:"unit"`
 	SiteID        uint   `json:"site"`
 }
